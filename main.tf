23cf2f4238f0b483f1fd88e57453780ced74b287
rxk41 <107802280+rxk41@users.noreply.github.com>
2025-03-24 10:35:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,16 @@ resource "google_project_service" "compute" {
   service = "compute.googleapis.com"
 }
 
+resource "google_cloudfunctions_function" "hello_world" {
+  name        = "hello-world-function"
+  description = "A simple Hello World Cloud Function"
+  runtime     = "nodejs16"
+  entry_point = "helloWorld"
+  source_archive_bucket = google_storage_bucket.function_bucket.name
+  source_archive_object = google_storage_bucket_object.function_archive.name
+  trigger_http = true
+}
+
 
 resource "google_storage_bucket" "function_bucket" {
   name     = "hello-world-function-bucket"
