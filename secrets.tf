c4d950cae4352b94c980ae9a24989f25904c3e44
rxk41 <107802280+rxk41@users.noreply.github.com>
2025-03-24 10:39:20 +0530

    Update secrets.tf

--- secrets.tf
+++ secrets.tf
@@ -9,3 +9,10 @@ resource "google_secret_manager_secret_version" "api_key_secret_version" {
   secret     = google_secret_manager_secret.api_key_secret.id
   secret_data = "Key.json"
 }
+
+# access secrets
+
+resource "google_project_iam_member" "function_secret_access" {
+  project = "devops-374804"
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_cloudfunctions_function.hello_world.service_account_email}"
